6f195f48c2afef5cbd34967dc296dfc8d98d7196
DevExpressExampleBot <fakeEmail>
2022-12-18 18:07:43 +0400

    Source auto update [skip ci]

---
+++ VB/WpfMapControl_ShapeTitleOptions/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace WpfMapControl_ShapeTitleOptions
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
